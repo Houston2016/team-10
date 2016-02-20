b6bf6f9bf00615ca2ca308f090e10b82c104ddce
Dargeren <ncartagena@iceoj.com>
2016-02-19 21:20:53 -0600

    update

---
+++ Flip/Assets/Scipts/GameObjectScripts/MasterControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MasterControl : MonoBehaviour {
+
+	public static MasterControl control;
+	void Awake () {
+		control = this;
+		DontDestroyOnLoad (control);
+		LoadScene ("TitleScene");
+	}
+	public void LoadScene(string s){
+		SceneManager.LoadScene (s);
+	}
+
+}
---
+++ Flip/Assets/Scipts/GameObjectScripts/TitleControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TitleControl : MonoBehaviour {
+
+	//Image background;
+
+
+	public static TitleControl control;
+	void Awake(){
+		control = this;
+		//background = transform.FindChild("TitleBackground").GetComponent<Image>().
+
+	}
+}
